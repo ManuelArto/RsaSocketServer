7cc429433c508f5914a09dc039a9cec570e5806a
ManuelArto <manuelartociao@gmail.com>
2020-04-28 03:04:54 +0200

    Final commit

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import socket
 import json
 import rsa
+from binascii import hexlify
+import os
 import threading
 import sys
 
@@ -11,48 +13,73 @@ class Client:
 		self.ADDR = ADDR
 		self.username = username
 		self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.active_users = []
-		self.priv = 0
-		self.pub = 0
+		self.active_users = {}
+		self.generate_key()
+
+	def generate_key(self):
+		try:
+			with open(f'{self.username}/priv.key', 'rb') as f:
+				priv_file_content = f.read()
+			self.privkey = rsa.PrivateKey.load_pkcs1(priv_file_content)
+		except IOError:
+			os.mkdir(self.username)
+			(self.pubkey, self.privkey) = rsa.newkeys(512)
+			with open(f'{self.username}/pub.key', 'wb') as f:
+				f.write(self.pubkey.save_pkcs1())
+			with open(f'{self.username}/priv.key', 'wb') as f:
+				f.write(self.privkey.save_pkcs1())
+		finally:
+			with open(f'{self.username}/pub.key', 'rb') as f:
+				pub_file_content = f.read()
+			self.pubkey = pub_file_content.decode()
+
+	def send_broadcast(self, msg):
+		for receiver in self.active_users:
+			self.send(receiver, msg)
 
 	def send(self, receiver, msg):
-		data = {"sender": self.username, "receiver": receiver, "msg": msg}
+		pubkey_recv = rsa.key.PublicKey.load_pkcs1(self.active_users[receiver])
+		crypto = rsa.encrypt(msg, pubkey_recv)
+		data = {"sender": self.username, "receiver": receiver, "msg": crypto.hex()}
 		self.client.send(json.dumps(data).encode())
 
 	def update_users(self, users):
+		del users[self.username]
 		self.active_users = users
-		self.active_users.remove(self.username)
-		self.active_users.append("broadcast")
+
+	def init_data(self):
+		data = {"username": self.username, "pubkey": self.pubkey}
+		self.client.send(json.dumps(data).encode())
 
 	def listen(self):
-		self.client.send(self.username.encode())
+		self.init_data()
 		while True:
 			data = self.client.recv(1024).decode()
 			data = json.loads(data)
 			if "users" in data.keys():
 				self.update_users(data["users"])
 			else:
-				print(f"\n[NEW MESSAGE] {data['sender']}: {data['msg']} \nMessage: ", end="")
+				crypto = bytes.fromhex(data["msg"])
+				msg = rsa.decrypt(crypto, self.privkey).decode()
+				print(f"\n[NEW MESSAGE] {data['sender']}: {msg} \nMessage: ", end="")
 
 	def run(self):
 		print(f"[CONNECTION] Starting connection to {ADDR}")
-		try:
-			self.client.connect(self.ADDR)
-			thread = threading.Thread(target=self.listen)
-			thread.start()
-			while True:
-				msg = input("Message: ")
-				if msg != self.DISCONNECT_MESSAGE:
-					print(f"\n[ACTIVE USERS] {self.active_users}")
-					receiver = input("Receiver: ")
-					if receiver not in self.active_users:
-						print(f"not a valid username")
-						continue
-				self.send(receiver, msg)
-		except Exception as e:
-			print(e)
-			self.client.close()
-			exit(0)
+		self.client.connect(self.ADDR)
+		thread = threading.Thread(target=self.listen)
+		thread.start()
+		while True:
+			msg = input("Message: ")
+			if msg != self.DISCONNECT_MESSAGE:
+				print(f"\n[ACTIVE USERS] {list(self.active_users.keys()) + ['broadcast']}")
+				receiver = input("Receiver: ")
+				if receiver not in list(self.active_users.keys()) + ["broadcast"]:
+					print(f"not a valid username")
+					continue
+			if receiver == "broadcast":
+				self.send_broadcast(msg.encode())
+			else:
+				self.send(receiver, msg.encode())
 
 
 IP = socket.gethostbyname(socket.gethostname())
